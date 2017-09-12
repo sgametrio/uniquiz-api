562ed935258d672ddd5d0c2bfa090c75230bd5cd
Demetrio Carrara <carrarademetrio@gmail.com>
2017-09-12 17:04:35 +0200

    Add initial use of resources for major abstraction and better namespace
    Signed-off-by: Demetrio Carrara <carrarademetrio@gmail.com>

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,9 +2,85 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Question;
+use App\Http\Resources\FullQuestion as FullQuestionResource;
 use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Question::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Question $question)
+    {
+        return new FullQuestionResource($question);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Question $question)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Question $question)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Question $question)
+    {
+        //
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,21 +20,7 @@ Route::get("/", function () {
     return "It works! (laravel)";
 });
 
-Route::get("/questions", function () {
-    return Question::all();
-});
-
-Route::get("/questions/{question}", function (Question $question) {
-    // Example of dinamically hiding fields
-    //$question->makeHidden("created_at");
-    return $question;
-});
-
-Route::get("/answers", function () {
-    return Answer::all();
-});
-
-Route::get("/answers/{answer}", function (Answer $answer) {
-    // Returning answer with its question
-    return $answer->with("question")->get();
-});
+Route::resource("/questions", "QuestionController");
+Route::resource("/quizzes", "QuizController");
+Route::resource("/answers", "AnswerController");
+Route::resource("/courses", "CourseController");
