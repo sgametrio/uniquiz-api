7f268b583ebe685c7bafc9d3232a83e458d26325
Demetrio Carrara <carrarademetrio@gmail.com>
2017-09-13 08:46:07 +0200

    Switch between web and API routes
    Signed-off-by: Demetrio Carrara <carrarademetrio@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
+| Every route has "/api" as prefix.
 |
 */
 
@@ -23,3 +24,6 @@ Route::resource("/admin/questions", "QuestionController");
 
 /***    WEBAPP API ROUTES    ***/
 Route::resource("/questions", "ApiQuestionController");
+Route::resource("/quizzes", "ApiQuizController");
+Route::resource("/answers", "ApiAnswerController");
+Route::resource("/courses", "ApiCourseController");
--- routes/web.php
+++ routes/web.php
@@ -11,24 +11,6 @@
 |
 */
 
-use App\Models\Question;
-use App\Models\Course;
-use App\Models\Quiz;
-use App\Models\Answer;
-
 Route::get("/", function () {
     return "It works! (laravel)";
 });
-
-/***    CRUD ADMIN ROUTES    ***/
-// TODO: add auth middleware
-Route::resource("/questions", "QuestionController");
-Route::resource("/quizzes", "QuizController");
-Route::resource("/answers", "AnswerController");
-Route::resource("/courses", "CourseController");
-
-/***    API ROUTES FOR WEB APP    ***/
-//Route::resource("/api/questions", "ApiQuestionController");
-Route::resource("/api/quizzes", "ApiQuizController");
-Route::resource("/api/answers", "ApiAnswerController");
-Route::resource("/api/courses", "ApiCourseController");
